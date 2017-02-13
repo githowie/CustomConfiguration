073ec697bebe9ffd8305b16aa1876503359951ff
howie <misc.howardc@gmail.com>
2017-02-12 20:24:31 -0500

    first commit

---
+++ CustomConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+
+namespace CustomConfiguration
+{
+    public class CustomConfiguration : ConfigurationSection
+    {
+        public static CustomConfiguration _Config = ConfigurationManager.GetSection("adminion_settings") as CustomConfiguration;
+        
+        public static CustomConfiguration GetSetting
+        {
+            get
+            {
+                return _Config;
+            }
+        }
+    }
+}
---
+++ Test.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace CustomConfiguration
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            
+            /*
+            string devUrl = string.Empty;
+            var connectionManagerDatabaseServers = ConfigurationManager.GetSection("ConnectionManagerDatabaseServers") as NameValueCollection;
+            if (connectionManagerDatabaseServers != null)
+            {
+                devUrl = connectionManagerDatabaseServers["Dev"].ToString();
+                Response.Write(devUrl);
+            }
+             * */
+
+            // Grab the Environments listed in the App.config and add them to our list.
+            var connectionManagerDatabaseServers = ConfigurationManager.GetSection("ConnectionManagerDatabaseServers") as NameValueCollection;
+            if (connectionManagerDatabaseServers != null)
+            {
+                foreach (var serverKey in connectionManagerDatabaseServers.AllKeys)
+                {
+                    string serverValue = connectionManagerDatabaseServers.GetValues(serverKey).FirstOrDefault();
+                    Response.Write(serverValue + "<BR>");
+                }
+            }
+            
+        }
+    }
+}
